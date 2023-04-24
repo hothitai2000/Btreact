52c27f57e4b76eb10d4506401cff995d4c7251cd
unknown <Tai Xoan@pnvl109.pn.com.vn>
2023-04-24 12:10:18 +0700

    doing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Component/App/App';
 import reportWebVitals from './reportWebVitals';
+// import Score from './Component/Hooks/Form';
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Score></Score> */}
   </React.StrictMode>
 );
 
